8621ac7e1c990dd85bb6393dea5a39a2b944a19a
jgnacio <jgnaciogomez@gmail.com>
2024-10-14 09:34:30 -0300

    refactor: Update ListOrders component to display order status and add loading spinner

--- src/app/dashboard/components/ListOrders.tsx
+++ src/app/dashboard/components/ListOrders.tsx
@@ -10,9 +10,10 @@ import {
 import { WooOrder } from "@/Resources/API/ASLAN/entities/AslanWooAPI";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { Spinner } from "@nextui-org/spinner";
 
 export default function ListOrders({ wooOrders }: { wooOrders: WooOrder[] }) {
-  return wooOrders ? (
+  return (
     <Card x-chunk="dashboard-01-chunk-5">
       <CardHeader>
         <CardTitle>Ultimos Pedidos</CardTitle>
@@ -21,57 +22,97 @@ export default function ListOrders({ wooOrders }: { wooOrders: WooOrder[] }) {
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-8">
-        <ScrollArea className="h-72 rounded-xl border">
-          <div className="p-4">
-            {wooOrders.map((order) => (
-              <>
-                <div key={order.id} className="flex items-center gap-4">
-                  <Avatar className="hidden h-9 w-9 sm:flex">
-                    <AvatarImage src="/avatars/01.png" alt="Avatar" />
-                    <AvatarFallback>
-                      {order.billing.first_name[0].toUpperCase()}
-                      {order.billing.last_name
-                        ? order.billing.last_name[0].toUpperCase()
-                        : ""}
-                    </AvatarFallback>
-                  </Avatar>
-                  <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                      {order.billing.first_name} {order.billing.last_name}
-                    </p>
-                    <p className="text-sm text-muted-foreground">
-                      {order.billing.email}
-                    </p>
+        {wooOrders ? (
+          <ScrollArea className="h-72 rounded-xl border">
+            <div className="p-4">
+              {wooOrders.map((order) => (
+                <>
+                  <div key={order.id} className="flex items-center gap-4">
+                    <Avatar className="hidden h-9 w-9 sm:flex">
+                      <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                      <AvatarFallback>
+                        {order.billing.first_name[0].toUpperCase()}
+                        {order.billing.last_name
+                          ? order.billing.last_name[0].toUpperCase()
+                          : ""}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="grid gap-1">
+                      <p className="text-sm font-medium leading-none">
+                        {order.billing.first_name} {order.billing.last_name}
+                      </p>
+                      <p className="text-sm text-muted-foreground">
+                        {order.billing.email}
+                      </p>
+                    </div>
+                    <div className="flex flex-col items-end ml-auto font-medium">
+                      {order.status === "completed" && (
+                        <>
+                          <div className="text-success-500">
+                            +${order.total}
+                          </div>
+                          <Badge variant="outlineSuccess">Completado</Badge>
+                        </>
+                      )}
+                      {order.status === "processing" && (
+                        <>
+                          <div className="text-warning-500">${order.total}</div>
+                          <Badge variant="outlineWarning">Procesando</Badge>
+                        </>
+                      )}
+                      {order.status === "cancelled" && (
+                        <>
+                          <div className="text-destructive">${order.total}</div>
+                          <Badge variant="outlineDestructive">Cancelado</Badge>
+                        </>
+                      )}
+
+                      {order.status === "pending" && (
+                        <>
+                          <div className="text-warning-500">${order.total}</div>
+                          <Badge variant="outlineWarning">Pendiente</Badge>
+                        </>
+                      )}
+                      {order.status === "on-hold" && (
+                        <>
+                          <div className="text-warning-500">${order.total}</div>
+                          <Badge variant="outlineWarning">En espera</Badge>
+                        </>
+                      )}
+
+                      {order.status === "refunded" && (
+                        <>
+                          <div className="text-destructive">
+                            -${order.total}
+                          </div>
+                          <Badge variant="outlineDestructive">
+                            Reembolsado
+                          </Badge>
+                        </>
+                      )}
+                      {order.status === "failed" && (
+                        <>
+                          <div className="text-destructive">${order.total}</div>
+                          <Badge variant="outlineDestructive">Fallido</Badge>
+                        </>
+                      )}
+                      {order.status === "trash" && (
+                        <>
+                          <div className="text-muted">${order.total}</div>
+                          <Badge variant="outline">Borrado</Badge>
+                        </>
+                      )}
+                    </div>
                   </div>
-                  <div className="flex flex-col items-end ml-auto font-medium">
-                    {order.status === "completed" && (
-                      <>
-                        <div className="text-success-500">+${order.total}</div>
-                        <Badge variant="outlineSuccess">Completado</Badge>
-                      </>
-                    )}
-                    {order.status === "processing" && (
-                      <>
-                        <div className="text-warning-500">${order.total}</div>
-                        <Badge variant="outlineWarning">Procesando</Badge>
-                      </>
-                    )}
-                    {order.status === "cancelled" && (
-                      <>
-                        <div className="text-destructive">${order.total}</div>
-                        <Badge variant="outlineDestructive">Cancelado</Badge>
-                      </>
-                    )}
-                  </div>
-                </div>
-                <Separator className="my-2" />
-              </>
-            ))}
-          </div>
-        </ScrollArea>
+                  <Separator className="my-2" />
+                </>
+              ))}
+            </div>
+          </ScrollArea>
+        ) : (
+          <Spinner size="lg" />
+        )}
       </CardContent>
     </Card>
-  ) : (
-    "Loading..."
   );
 }
